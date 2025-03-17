34d14aebc910b6e83494782f2f6d000b7666a6ac
shabarishoganb <131852273+shabarishoganb@users.noreply.github.com>
2025-03-17 22:18:18 +0530

    Create 5.py

---
+++ Tutorial 1/5.py
@@ -0,0 +1,4 @@
+import math
+a, b, c = float(input()), float(input()), float(input())
+d = b**2 - 4*a*c
+print("No real roots" if d < 0 else (-b + math.sqrt(d)) / (2*a), (-b - math.sqrt(d)) / (2*a))
